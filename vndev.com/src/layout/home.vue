27c1499b2c7eff15fe32d1d1897b65012c9231ad
TaiPhan <tai.phan@bigin.vn>
2020-01-22 11:55:50 +0700

    creat project

---
+++ vndev.com/src/layout/home.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+ 
+</div>
+</template>
+
+<script>
+console.log('ok');
+import Blogheader from './header'
+import BlogFooter from './footer'
+
+export default {
+  name: 'home',
+   components: { Blogheader, BlogFooter },
+  data () {
+    return {
+      msg: 'home'
+    }
+  }
+}
+</script>
